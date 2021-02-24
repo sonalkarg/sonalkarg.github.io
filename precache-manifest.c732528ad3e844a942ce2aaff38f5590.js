e183256c069071441d245138b96bf3af08d34484
sonalkarg <gauras_21@rediffmail.com>
2021-02-24 16:25:10 +0530

    Add files via upload

---
+++ precache-manifest.c732528ad3e844a942ce2aaff38f5590.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047be82c5c4d3e6c2d7cec684df09817",
+    "url": "/index.html"
+  },
+  {
+    "revision": "446c5a722295c39681fa",
+    "url": "/static/css/main.4c230d4a.chunk.css"
+  },
+  {
+    "revision": "0d2d9f06ad20dd3ce77c",
+    "url": "/static/js/2.0242b80a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.0242b80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446c5a722295c39681fa",
+    "url": "/static/js/main.dd007066.chunk.js"
+  },
+  {
+    "revision": "f3e3d39c6956ba81638d",
+    "url": "/static/js/runtime-main.730ad540.js"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "9ce2430429b509dd193210f983b7a480",
+    "url": "/static/media/gautam.9ce24304.jpg"
+  },
+  {
+    "revision": "ffcc99c42164b65da7af6307b66379cf",
+    "url": "/static/media/header.ffcc99c4.jpg"
+  },
+  {
+    "revision": "a76c7b4ce725bb07b1572699a5a78c62",
+    "url": "/static/media/hero-image-1.a76c7b4c.png"
+  },
+  {
+    "revision": "9616730d099782a3ea6c12c70aa181af",
+    "url": "/static/media/hero-image-2.9616730d.png"
+  },
+  {
+    "revision": "02cd0e2d2b27926d1ed5c31a00dd26fa",
+    "url": "/static/media/hero-image-3.02cd0e2d.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
